6d486ad458c61c3e51a1354d691ac89076ea5809
Vincent_Liussyaputra <liusvinv@gmail.com>
2024-01-08 03:46:49 +0700

    Add footer styling

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -4,8 +4,10 @@ type Props = {}
 
 export default function Footer({}: Props) {
 	return (
-		<footer>
-			Website Design by <b>Shubamium</b> -  PinBun VT &copy;2024
+		<footer id='container_footer'>
+			<p>
+				Website Design by <a href='https://twitter.com/Shubamium2' target='_blank'><b>Shubamium</b></a> -  PinBun VT &copy;2024
+			</p>
 		</footer>
 	)
 }
