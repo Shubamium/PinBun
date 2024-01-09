c54c1e8e336759d659c7cf1edb27f22f57c0a2cc
Vincent_Liussyaputra <liusvinv@gmail.com>
2024-01-10 01:21:11 +0700

    Add graphic section in gallery

--- src/app/gallery/graphics/page.tsx
+++ src/app/gallery/graphics/page.tsx
@@ -1,11 +1,123 @@
 import React from 'react'
 import './galleryGraphics.scss'
+import { FaHeart } from 'react-icons/fa'
 type Props = {}
 
 export default function page({}: Props) {
 	return (
 		<div id='gallery_graphics'>
+				<div className="graphic-list">
+					<figure className="graphic">
+							<div className="decors">
+									<img src="/images/decorations/bunny_closed.png" alt="" className='decor bunny' />
+									<img src="/images/decorations/pyon.png" alt="" className='decor pyon' />
+									<img src="/images/decorations/carrot.png" alt="" className='decor carrot' />
+							</div>
+							<div className="info">
+								<h2> <FaHeart/>Graphic Name</h2>
+								<a href="#" className='credit'>@person_name</a>
+							</div>
+							<div className="graphic-part">
+								<img src="/images/placeholders/graphic.png" alt="" className='main_graphic' />
+							</div>
+					</figure>
 				
+					<figure className="graphic">
+							<div className="decors">
+									<img src="/images/decorations/bunny_closed.png" alt="" className='decor bunny' />
+									<img src="/images/decorations/pyon.png" alt="" className='decor pyon' />
+									<img src="/images/decorations/carrot.png" alt="" className='decor carrot' />
+							</div>
+							<div className="info">
+								<h2> <FaHeart/>Graphic Name</h2>
+								<a href="#" className='credit'>@person_name</a>
+							</div>
+							<div className="graphic-part">
+								<img src="/images/placeholders/graphic.png" alt="" className='main_graphic' />
+							</div>
+					</figure>
+				
+
+					<figure className="graphic">
+							<div className="decors">
+									<img src="/images/decorations/bunny_closed.png" alt="" className='decor bunny' />
+									<img src="/images/decorations/pyon.png" alt="" className='decor pyon' />
+									<img src="/images/decorations/carrot.png" alt="" className='decor carrot' />
+							</div>
+							<div className="info">
+								<h2> <FaHeart/>Graphic Name</h2>
+								<a href="#" className='credit'>@person_name</a>
+							</div>
+							<div className="graphic-part">
+								<img src="/images/placeholders/graphic.png" alt="" className='main_graphic' />
+							</div>
+					</figure>
+				
+					<figure className="graphic">
+							<div className="decors">
+									<img src="/images/decorations/bunny_closed.png" alt="" className='decor bunny' />
+									<img src="/images/decorations/pyon.png" alt="" className='decor pyon' />
+									<img src="/images/decorations/carrot.png" alt="" className='decor carrot' />
+							</div>
+							<div className="info">
+								<h2> <FaHeart/>Graphic Name</h2>
+								<a href="#" className='credit'>@person_name</a>
+							</div>
+							<div className="graphic-part">
+								<img src="/images/placeholders/graphic.png" alt="" className='main_graphic' />
+							</div>
+					</figure>
+				
+
+					<figure className="graphic">
+							<div className="decors">
+									<img src="/images/decorations/bunny_closed.png" alt="" className='decor bunny' />
+									<img src="/images/decorations/pyon.png" alt="" className='decor pyon' />
+									<img src="/images/decorations/carrot.png" alt="" className='decor carrot' />
+							</div>
+							<div className="info">
+								<h2> <FaHeart/>Graphic Name</h2>
+								<a href="#" className='credit'>@person_name</a>
+							</div>
+							<div className="graphic-part">
+								<img src="/images/placeholders/graphic.png" alt="" className='main_graphic' />
+							</div>
+					</figure>
+				
+
+					<figure className="graphic">
+							<div className="decors">
+									<img src="/images/decorations/bunny_closed.png" alt="" className='decor bunny' />
+									<img src="/images/decorations/pyon.png" alt="" className='decor pyon' />
+									<img src="/images/decorations/carrot.png" alt="" className='decor carrot' />
+							</div>
+							<div className="info">
+								<h2> <FaHeart/>Graphic Name</h2>
+								<a href="#" className='credit'>@person_name</a>
+							</div>
+							<div className="graphic-part">
+								<img src="/images/placeholders/graphic.png" alt="" className='main_graphic' />
+							</div>
+					</figure>
+				
+
+					<figure className="graphic">
+							<div className="decors">
+									<img src="/images/decorations/bunny_closed.png" alt="" className='decor bunny' />
+									<img src="/images/decorations/pyon.png" alt="" className='decor pyon' />
+									<img src="/images/decorations/carrot.png" alt="" className='decor carrot' />
+							</div>
+							<div className="info">
+								<h2> <FaHeart/>Graphic Name</h2>
+								<a href="#" className='credit'>@person_name</a>
+							</div>
+							<div className="graphic-part">
+								<img src="/images/placeholders/graphic.png" alt="" className='main_graphic' />
+							</div>
+					</figure>
+				
+				
+				</div>
 		</div>
 	)
 }
