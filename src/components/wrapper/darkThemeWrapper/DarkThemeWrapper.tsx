a99eeb2eff5831b32e55cf908d2bd3b65097778f
Vincent_Liussyaputra <liusvinv@gmail.com>
2024-05-27 22:48:16 +0700

    Change the duration

--- src/components/wrapper/darkThemeWrapper/DarkThemeWrapper.tsx
+++ src/components/wrapper/darkThemeWrapper/DarkThemeWrapper.tsx
@@ -1,61 +1,68 @@
-'use client'
-import { color } from 'console-log-colors';
-import React, { CSSProperties, useEffect, useState } from 'react'
+"use client";
+import { color } from "console-log-colors";
+import React, { CSSProperties, useEffect, useState } from "react";
 
 type Props = {
-	children:React.ReactNode,
-	className:string,
-	style:CSSProperties
-}
+  children: React.ReactNode;
+  className: string;
+  style: CSSProperties;
+};
 
-export default function DarkThemeWrapper({children,className,style}: Props) {
-	const [theme,setTheme] = useState(true);
-	
-	useEffect(()=>{
-		// return
-		const intervalId = setInterval(()=>{
-			setTheme(false)
-			console.log('switching theme : '+ color.black('dark'))
+export default function DarkThemeWrapper({
+  children,
+  className,
+  style,
+}: Props) {
+  const [theme, setTheme] = useState(true);
 
-			const glitchedTitles = [
-				`👁P̷i̷n̵ ̴b̵u̴n̸`,
-				`P̴̫͂í̵̟n̵̢̆ ̷̤̎b̶͕̈́ȗ̷̥n̶͙̆👁`,
-				`P̵̤͍̬̃i̸̟̘̝̒͆̈👁n̷̡͔̤̍͛ ̴̡̛̻́͋̑b̵͉͖̰̾u̵͍̍n̶̨̳̈̎`,
-				`P̷̧̔̽̽̅̏̔͠i̸͉̗͚̲̘̳̖͒̊̔̚͘͘̚n̶̢̩̰̈́ ̷̧̋͂͘b̶̪̩̊͌👁u̶̧̞̳͍̜̝̓͋̌͜n̸̑̎͒̊͐̚͜͝`,
-				`P̸̧͙̗͈͕͈̯̮̤͍̹̝͍̰̀͐̓̀̀͌͂̈́͠͠👁ǐ̷̢̾̄̈n̴̛̦̦͇̩͕̘̥̯̲͋̅̀͂̎̈́́̔͆͘ ̴̛̹͊͌͐̉́̎͗͗̌͗͗̚b̴̡̞̳̮̭̲̗̤͙͔̲͙̓ü̴̜͉̰͓̗̠̖̟̲̩̺̍̏̅͆͊̾̃̋̈́̚͠n̵̼͓̯̻̟̖͕̞̖͖̔̕`,
-				`P̴̨̡̧̥̫̞̲̘̲͎͈̣̥̘̟͊̅̎͆̑́̑̈̎̃̀̾͛͘͝͠î̵͚̩̲͖̩͎̩̞̖̼͓͈̠̼̳̑̈̑̌̃̌̅̊̚̕͘͜͝ṅ̷̨̛̘̱̞̦̺͔̜̺̂̀̀̑̀͛̋͆͗̏͌̆̉́̚͜ ̸͓̳̪̜̅̎̃͐̋̌̃̎̎́̓͊͝͝b̵̳̀͗͌̓́̂̄͌̑̉̕👁ừ̴̛͇̮̜̣̒́̐̐́́̈̒̈́̃́̿̿͠͝͠͝ͅn̸̢͉̗̘͔̼̤͚̈́͛̈́̊͊͆̉̒̉́̈́̑̾̓͘͝͝
-				̶̡̟̘̞̐͊̏̽̽̾͊͠`,`
+  useEffect(() => {
+    // return
+    const intervalId = setInterval(() => {
+      setTheme(false);
+      console.log("switching theme : " + color.black("dark"));
+
+      const glitchedTitles = [
+        `👁P̷i̷n̵ ̴b̵u̴n̸`,
+        `P̴̫͂í̵̟n̵̢̆ ̷̤̎b̶͕̈́ȗ̷̥n̶͙̆👁`,
+        `P̵̤͍̬̃i̸̟̘̝̒͆̈👁n̷̡͔̤̍͛ ̴̡̛̻́͋̑b̵͉͖̰̾u̵͍̍n̶̨̳̈̎`,
+        `P̷̧̔̽̽̅̏̔͠i̸͉̗͚̲̘̳̖͒̊̔̚͘͘̚n̶̢̩̰̈́ ̷̧̋͂͘b̶̪̩̊͌👁u̶̧̞̳͍̜̝̓͋̌͜n̸̑̎͒̊͐̚͜͝`,
+        `P̸̧͙̗͈͕͈̯̮̤͍̹̝͍̰̀͐̓̀̀͌͂̈́͠͠👁ǐ̷̢̾̄̈n̴̛̦̦͇̩͕̘̥̯̲͋̅̀͂̎̈́́̔͆͘ ̴̛̹͊͌͐̉́̎͗͗̌͗͗̚b̴̡̞̳̮̭̲̗̤͙͔̲͙̓ü̴̜͉̰͓̗̠̖̟̲̩̺̍̏̅͆͊̾̃̋̈́̚͠n̵̼͓̯̻̟̖͕̞̖͖̔̕`,
+        `P̴̨̡̧̥̫̞̲̘̲͎͈̣̥̘̟͊̅̎͆̑́̑̈̎̃̀̾͛͘͝͠î̵͚̩̲͖̩͎̩̞̖̼͓͈̠̼̳̑̈̑̌̃̌̅̊̚̕͘͜͝ṅ̷̨̛̘̱̞̦̺͔̜̺̂̀̀̑̀͛̋͆͗̏͌̆̉́̚͜ ̸͓̳̪̜̅̎̃͐̋̌̃̎̎́̓͊͝͝b̵̳̀͗͌̓́̂̄͌̑̉̕👁ừ̴̛͇̮̜̣̒́̐̐́́̈̒̈́̃́̿̿͠͝͠͝ͅn̸̢͉̗̘͔̼̤͚̈́͛̈́̊͊͆̉̒̉́̈́̑̾̓͘͝͝
+				̶̡̟̘̞̐͊̏̽̽̾͊͠`,
+        `
 				👁^v^v^👁
-				`
-			]
+				`,
+      ];
 
-			for(let i = 0; i < glitchedTitles.length;i++){
-				setTimeout(() => {
-					document.title =  glitchedTitles[Math.floor(Math.random()*glitchedTitles.length)]
-				}, i*50);
-			}
+      for (let i = 0; i < glitchedTitles.length; i++) {
+        setTimeout(() => {
+          document.title =
+            glitchedTitles[Math.floor(Math.random() * glitchedTitles.length)];
+        }, i * 50);
+      }
 
-			setTimeout(()=>{
-				document.title = `y̴̭̩̰͎͖̹͖̗͍̺̾̈́͒́́̈̾͐̅̕͘o̶̯̮͔̘̳̫̘̹̰̭̎͐̅̈̾̎ͅu̸̢̡͚͔̳̻̲͛̇̔͛̍̄͊̊̅͊͘͜͠r̸̡̨̯̳̟̞̠̟͈̫̒͐͝ ̵̻̪̻̭̯͈̬̟̦͆̌̓̐b̵͓͎͚̃̐͋͐͒̍̈́̒͒ë̴̛̟̠̖̱͕͍͇̮̤̖̱̮͔̭́̑ţ̴̰͌̊͛̌̔ẗ̴̨̪̟̞̖̲̜̰͈̝̼͚̼͇̤́͗̉̃̔̏̕͠ė̵̢̧̯̣̩͉͉̭͍͇͓͗͜ŗ̷̨̢̨̩̯̮͉̺͓͔̞̔̈́̀͋̉͑̓̚ ̴̨̨͎̲̝͈̎͐͐͜͝m̵̢̙̩̱̫̠̦̺̗͙̥̟͆̆̿̈̋̈́̚͘͘ǫ̴̤̘͉͙͇̮̫̎͌́̓͜͜ţ̸̡̟͚̲̤͈͚̦̟̠͎̘͍͌h̵̡̞̥͍̞͈̹͑́̈́̔̿͌̀̏͂́͐̆̌͜ȩ̴̛̹̙̪͇͙̯̭̃̏̈́͗͋̍̓̀̈́̄̌̀͜͝r̶̹͙̘̬͚͍͌̐̽͛͌̀̅́̋̔`
-			},2000)
-		
-			setTimeout(() => {
-				setTheme(true)
-				document.title = 'Pin Bun'
-			},3500)
-			console.log('switching theme : '+ color.redBright('light'))
+      setTimeout(() => {
+        document.title = `y̴̭̩̰͎͖̹͖̗͍̺̾̈́͒́́̈̾͐̅̕͘o̶̯̮͔̘̳̫̘̹̰̭̎͐̅̈̾̎ͅu̸̢̡͚͔̳̻̲͛̇̔͛̍̄͊̊̅͊͘͜͠r̸̡̨̯̳̟̞̠̟͈̫̒͐͝ ̵̻̪̻̭̯͈̬̟̦͆̌̓̐b̵͓͎͚̃̐͋͐͒̍̈́̒͒ë̴̛̟̠̖̱͕͍͇̮̤̖̱̮͔̭́̑ţ̴̰͌̊͛̌̔ẗ̴̨̪̟̞̖̲̜̰͈̝̼͚̼͇̤́͗̉̃̔̏̕͠ė̵̢̧̯̣̩͉͉̭͍͇͓͗͜ŗ̷̨̢̨̩̯̮͉̺͓͔̞̔̈́̀͋̉͑̓̚ ̴̨̨͎̲̝͈̎͐͐͜͝m̵̢̙̩̱̫̠̦̺̗͙̥̟͆̆̿̈̋̈́̚͘͘ǫ̴̤̘͉͙͇̮̫̎͌́̓͜͜ţ̸̡̟͚̲̤͈͚̦̟̠͎̘͍͌h̵̡̞̥͍̞͈̹͑́̈́̔̿͌̀̏͂́͐̆̌͜ȩ̴̛̹̙̪͇͙̯̭̃̏̈́͗͋̍̓̀̈́̄̌̀͜͝r̶̹͙̘̬͚͍͌̐̽͛͌̀̅́̋̔`;
+      }, 2000);
 
-		},10*1000)
+      setTimeout(() => {
+        setTheme(true);
+        document.title = "Pin Bun";
+      }, 4000);
+      console.log("switching theme : " + color.redBright("light"));
+    }, 10 * 2500);
 
-		return () => {
-			clearInterval(intervalId)
-		}
-		
-	},[])
-	
-	return (
-		<body className={className + `-theme ${!theme ? 'dark' : 'light'} `} style={style}>
-			{children}
-		</body>
-	)
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <body
+      className={className + `-theme ${!theme ? "dark" : "light"} `}
+      style={style}
+    >
+      {children}
+    </body>
+  );
 }
